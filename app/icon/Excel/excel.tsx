82e3b219db2b20a14a44c70d87dcc637dd199c83
Raben Kiady <rabenjatovokex@gmail.com>
2026-05-13 19:40:12 +0300

    milay ee

--- app/icon/Excel/excel.tsx
+++ app/icon/Excel/excel.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
-const Excelicons = (props) => (
+import { SVGProps } from "react"
+const Excelicons = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
--- app/icon/Word/word.tsx
+++ app/icon/Word/word.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
+import { SVGProps } from "react"
 
-const Wordicons = (props) => (
+const Wordicons = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
